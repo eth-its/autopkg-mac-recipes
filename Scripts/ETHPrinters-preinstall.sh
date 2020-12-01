aeb42de5409728a4e86b4d5a3a08f3f1c525365a
Graham R Pugh <graham.pugh@id.ethz.ch>
2020-12-01 15:35:50 +0100

    new ETH printers recipe

--- Scripts/ETHPrinters-preinstall.sh
+++ Scripts/ETHPrinters-preinstall.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # install a fresh copy of each of the printer drivers
 jamf policy -event "ETH Printer Drivers HP S-install" -forceNoRecon
@@ -7,3 +7,9 @@ jamf policy -event "ETH Printer Drivers Ricoh Vol3-install" -forceNoRecon
 jamf policy -event "ETH Printer Drivers Ricoh Vol4-install" -forceNoRecon
 
 # delete the old version of the app if it is installed
+if [[ -d "/Applications/Utilities/ETH Printers.app" ]]; then
+    rm -Rf "/Applications/Utilities/ETH Printers.app"
+    # forget package
+    pkgutil --pkgs=ch.ethz.ethprinters.pkg && pkgutil --forget ch.ethz.ethprinters.pkg ||:
+fi
+
