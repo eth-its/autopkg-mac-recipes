73fb399de28542e1dbeca69d6e6d28ec4f94e5bf
Graham R Pugh <graham.pugh@id.ethz.ch>
2020-12-01 18:12:13 +0100

    ETH Printers needs special uninstaller script since it is not in Applications folder

--- Scripts/ETHPrinters-uninstall.sh
+++ Scripts/ETHPrinters-uninstall.sh
@@ -10,12 +10,7 @@
 
 function silent_app_quit() {
     # silently kill the application.
-    # add .app to end of string if not supplied
-    app_name="${app_name/\.app/}"            # remove any .app
-    check_app_name="${app_name/\(/\\(}"       # escape any brackets for the pgrep
-    check_app_name="${check_app_name/\)/\\)}"  # escape any brackets for the pgrep
-    check_app_name="${check_app_name}.app"     # add the .app back
-    if pgrep -f "/${check_app_name}" ; then
+    if pgrep -f "/$app_name" ; then
         echo "Closing $app_name"
         /usr/bin/osascript -e "quit app \"$app_name\"" &
         sleep 1
@@ -23,7 +18,7 @@ function silent_app_quit() {
         # double-check
         n=0
         while [[ $n -lt 10 ]]; do
-            if pgrep -f "$check_app_name" ; then
+            if pgrep -f "/$app_name" ; then
                 (( n=n+1 ))
                 sleep 1
 				echo "Graceful close attempt # $n"
@@ -32,20 +27,15 @@ function silent_app_quit() {
                 break
             fi
         done
-        if pgrep -f "$check_app_name" ; then
+        if pgrep -f "/$app_name" ; then
             echo "$app_name failed to quit - killing."
-            /usr/bin/pkill -f "$check_app_name"
+            /usr/bin/pkill -f "$app_name"
         fi
     fi
 }
 
 # Inputted variables
-app_name="%JSS_INVENTORY_NAME%"
-
-if [[ -z "${app_name}" ]]; then
-    echo "No application specified!"
-    exit 1
-fi
+app_name="ETH Printers.app"
 
 # quit the app if running
 silent_app_quit "$app_name"
@@ -54,21 +44,12 @@ silent_app_quit "$app_name"
 # Now remove the app
 echo "Removing application: ${app_name}"
 
-# Add .app to end when providing just a name e.g. "TeamViewer"
-if [[ ! $app_name == *".app"* ]]; then
-	app_name=$app_name".app"
-fi
-
 # Add standard path if none provided
-if [[ ! $app_name == *"/"* ]]; then
-	app_to_trash="/Applications/$app_name"
-else
-	app_to_trash="$app_name"
-fi
+app_to_trash="/Applications/Utilities/$app_name"
 
 echo "Application will be deleted: $app_to_trash"
 # Remove the application
-/bin/rm -rf "${app_to_trash}"
+/bin/rm -Rf "${app_to_trash}"
 
 echo "Checking if $app_name is actually deleted..."
 if [[ -d "${app_to_trash}" ]]; then
