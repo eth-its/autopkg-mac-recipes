6fc7d793f2368c0e289207c4dd2c2992b2170932
Graham R Pugh <graham.pugh@id.ethz.ch>
2021-01-05 09:08:07 +0100

    try out some changes to fix the installation of ETHPrinters

--- Scripts/ETHPrinters-preinstall.sh
+++ Scripts/ETHPrinters-preinstall.sh
@@ -9,7 +9,15 @@ jamf policy -event "ETH Printer Drivers Ricoh Vol4-install" -forceNoRecon
 # delete the old version of the app if it is installed
 if [[ -d "/Applications/Utilities/ETH Printers.app" ]]; then
     rm -Rf "/Applications/Utilities/ETH Printers.app"
-    # forget package
-    pkgutil --pkgs=ch.ethz.ethprinters.pkg && pkgutil --forget ch.ethz.ethprinters.pkg ||:
 fi
 
+# also delete any version of the app that was placed in a subfolder
+if [[ -d "/Applications/Utilities/ETH Printers" ]]; then
+    rm -Rf "/Applications/Utilities/ETH Printers"
+fi
+
+# forget package
+pkgutil --pkgs=ch.ethz.ethprinters.pkg && pkgutil --forget ch.ethz.ethprinters.pkg ||:
+
+# wait a few seconds to try and prevent the app getting installed in a subfolder
+sleep 5
