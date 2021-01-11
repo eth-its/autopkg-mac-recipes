9259303caa0c5026b5301d841f993c717f16e4a6
Graham R Pugh <graham.pugh@id.ethz.ch>
2021-01-11 13:07:10 +0100

    Add None to EA

--- Scripts/AdoptOpenJDK-EA.sh
+++ Scripts/AdoptOpenJDK-EA.sh
@@ -6,6 +6,7 @@ filter=%EXTENSION_ATTRIBUTE_VALUE%
 temp_file="/tmp/jamf_java_versions_extension_attribute_check.plist"
 /usr/libexec/java_home -X -v $filter > "$temp_file"
 latest_jvm=$(/usr/libexec/PlistBuddy -c "Print :0:JVMVersion" "$temp_file" 2>/dev/null)
+[[ $latest_jvm == "" ]] && latest_jvm="None"
 rm -f "$temp_file"
 echo "<result>$latest_jvm</result>"
 
