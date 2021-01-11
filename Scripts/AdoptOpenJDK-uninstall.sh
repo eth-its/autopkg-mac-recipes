d97b26043a1d596d5526ecf0c7a72ddd4d4db291
Graham R Pugh <graham.pugh@id.ethz.ch>
2021-01-11 15:40:11 +0100

    Adopt uninstall recipes attempt

--- Scripts/AdoptOpenJDK-uninstall.sh
+++ Scripts/AdoptOpenJDK-uninstall.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-filter=%SCRIPT_PARAMETER4%
-active_jvm=1
+filter="$4"
+has_jvm=1
 
-while [[ $active_jvm == 1 ]]; do
-    active_jvm_path=$(/usr/libexec/java_home -V 2>/dev/null | tail -n 1 | grep $filter | sed 's|/Contents/Home||')
-    if [[ -d "$active_jvm_path" ]]; then
-        echo "Deleting '$active_jvm_path'"
-        rm -rf "$active_jvm_path"
+if [[ $filter == "8" ]]; then 
+    filter_name="jdk8"
+elif [[ $filter == "11" ]]; then
+    filter_name="jdk-11"
+else
+    echo "No JVM version chosen"
+    exit 1
+fi
+
+while [[ $has_jvm == 1 ]]; do
+    jvm_path=$(find /Library/Java/JavaVirtualMachines -name "$filter_name*" -maxdepth 1 | head -n 1)
+    if [[ -d "$jvm_path" ]]; then
+        echo "Deleting '$jvm_path'"
+        rm -Rf "$jvm_path"
     else
-        echo "No active JVM"
-        active_jvm=0
+        echo "No JVM with version $filter is installed"
+        has_jvm=0
     fi
 done
-
-
